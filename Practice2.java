cd454848c646c7bfe8b289ca4c44cdbc0c0eb2c6
Hyo-gyeong <50789692+Hyo-gyeong@users.noreply.github.com>
2019-10-23 10:11:09 +0900

    변수이름
    세로 width -> height

--- Practice2.java
+++ Practice2.java
@@ -6,7 +6,7 @@ public class Practice2 {
 		int height = 20;
 		
 		System.out.println("가로 길이: " + width + "\n");
-		System.out.println("세로 길이: "+ width + "\n");
+		System.out.println("세로 길이: "+ height + "\n");
 		System.out.println("사각형의 넓이: "+ width * height + "\n");
 	}
 
